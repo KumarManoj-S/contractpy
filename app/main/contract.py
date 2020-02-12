dd67074d7b92012b464b6ef2e3edae4f597f76df
Manoj Kumar S <kumarmanoj1158@gmail.com>
2020-02-12 17:05:44 +0530

    Initial commit

---
+++ app/main/contract.py
@@ -0,0 +1,41 @@
+from app.main.exceptions.invalid_format import InvalidFormat
+from app.main.exceptions.invalid_value import InvalidValue
+
+
+class Types:
+    STRING = 'STRING'
+    INTEGER = 'INTEGER'
+    FLOAT = 'FLOAT'
+
+
+class Contract(object):
+    def __init__(self, contract):
+        self.__contract = contract
+
+
+
+class Contract:
+    @staticmethod
+    def _validate_format(contract):
+        valid_formats = [dict, list]
+        if type(contract) not in valid_formats:
+            raise InvalidFormat(
+                f"""Invalid contract format. Valid formats are 
+                    { ",".join(map(lambda x: x.__name__, valid_formats)) }.
+                """
+            )
+
+    @staticmethod
+    def _validate_contract(contract):
+        Contract._validate_format(contract)
+        if type(contract) == dict:
+            for key, value in contract.items():
+                if value not in [Types.FLOAT, Types.INTEGER, Types.STRING]:
+                    raise InvalidValue(
+                        f"""Invalid value for the field {key}. Valid values are 
+                            {', '.join(i for i in dir(Types) if not i.startswith('__'))}
+                        """
+                    )
+
+    def init(self, contract):
+        pass
