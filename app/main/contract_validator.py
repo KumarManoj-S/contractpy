2c28832360efda4c4c82c31a3886f13c116438dd
Manoj Kumar S <kumarmanoj1158@gmail.com>
2020-02-29 00:34:07 +0530

    Refcatores the code by replacing type with isinstance

--- app/main/contract_validator.py
+++ app/main/contract_validator.py
@@ -6,32 +6,30 @@ from app.main.types import Types
 class ContractValidator:
     @staticmethod
     def _validate_format(contract):
-        valid_formats = [dict, list]
-        if type(contract) not in valid_formats:
+        valid_formats = (dict, list)
+        if not isinstance(contract, valid_formats):
             raise InvalidFormat(
                 f"Invalid contract format. Valid formats are {','.join(map(lambda x: x.__name__, valid_formats)) }."
             )
 
     @staticmethod
     def _validate_contract_recursively(contract):
-        if type(contract) == list:
+        if isinstance(contract, list):
             if len(contract) != 1:
                 raise InvalidValue("More than one value cannot be passed to list")
             ContractValidator._validate_contract_recursively(contract[0])
-
-        elif type(contract) == dict:
+        elif isinstance(contract, dict):
             for key, value in contract.items():
-                if type(value) in [dict, list]:
+                if isinstance(value, (dict, list)):
                     ContractValidator._validate_contract_recursively(value)
-                else:
-                    if value not in [Types.FLOAT, Types.INTEGER, Types.STRING]:
-                        raise InvalidValue(
-                            f"Invalid value for the field {key}. Valid values are {', '.join(i for i in dir(Types) if not i.startswith('__'))}."
-                        )
+                elif value not in [Types.FLOAT, Types.INTEGER, Types.STRING]:
+                    raise InvalidValue(
+                        f"Invalid value for the field {key}. Valid values are {', '.join(i for i in dir(Types) if not i.startswith('__'))}."
+                    )
         elif contract not in [Types.FLOAT, Types.INTEGER, Types.STRING]:
             raise InvalidValue(
-                            f"Invalid values for the list. Valid values are {', '.join(i for i in dir(Types) if not i.startswith('__'))}"
-                        )
+                f"Invalid values for the list. Valid values are {', '.join(i for i in dir(Types) if not i.startswith('__'))}"
+            )
 
     @staticmethod
     def validate(contract):
