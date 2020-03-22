51c134778a3ec03ddd3c8d53ac25304383b74d0d
Manoj Kumar S <kumarmanoj1158@gmail.com>
2020-03-22 11:09:34 +0530

    Adds setup python file

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='pycontract',
+    packages=['pycontract'],
+    version='0.1',
+    license='MIT',
+    description='A tiny library for validating our data if its adhere the contract.',
+    author='Manoj Kumar S',
+    author_email='kumarmanoj1158@gmail.com',
+    url='https://github.com/KumarManoj-S/pycontract',
+    download_url='https://github.com/KumarManoj-S/pycontract',
+    keywords=['contract', 'contract testing', 'template', 'validate'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
